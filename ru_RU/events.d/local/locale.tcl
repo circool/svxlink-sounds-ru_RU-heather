50010128f225e6023f62a052cb404c0237713621
Vladimir <circool@mail.ru>
2025-03-06 15:29:17 +0300

    bug fix

--- ru_RU/events.d/local/locale.tcl
+++ ru_RU/events.d/local/locale.tcl
@@ -8,11 +8,11 @@ proc playTime {hour minute} {
 	variable Logic::CFG_TIME_FORMAT
 
 	# Проверка корректности формата времени
-	if {$hour < 0 || $hour > 24 || {[string length $hour] > 2} } {
+	if {$hour < 0 || $hour > 24 } {
 		puts "*** WARNING: Function playTime received invlalid argument hour: $hour (0-$CFG_TIME_FORMAT)"
 		return
 	}
-	if {$minute < 0 || $minute > 59 || {[string length $hour] > 2} } {
+	if {$minute < 0 || $minute > 59  } {
 		puts "*** WARNING: Function playTime received invlalid argument minute: $minute. Must be 0 до 59"
 		return
 	}
